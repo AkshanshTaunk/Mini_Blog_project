026906a1dd47f541ac71f7d62b4e5ce08efc22b0
akshansh <akshansh456@gmail.com>
2023-05-10 22:48:41 +0530

    signin,login,logout

--- miniblog/blog/views.py
+++ miniblog/blog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render,HttpResponseRedirect
-
+from django.contrib import messages
+from .forms import signupform, loginform
+from django.contrib.auth import authenticate,login,logout
+from .models import Post
 # Create your views here.
 def home(request):
-    return render(request,'blog/home.html')
+    posts = Post.objects.all()
+    return render(request,'blog/home.html',{'posts':posts})
 
 def about(request):
     return render(request,'blog/about.html')
@@ -14,10 +18,31 @@ def dashboard(request):
     return render(request,'blog/dashboard.html')
 
 def user_logout(request):
-    return HttpResponseRedirect('/') 
+    logout(request)
+    return HttpResponseRedirect ('/')
 
 def user_signup(request):
-    return render(request,'blog/signup.html')
+    if request.method =="POST":
+        fm = signupform(request.POST)
+        if fm.is_valid():
+            messages.success(request,'Congratulation!! You have become an Author')
+            fm.save()
+    else:
+        fm = signupform()
+    return render(request,'blog/signup.html',{'form':fm})
 
 def user_login(request):
-    return render(request,'blog/login.html')
+    if request.method == 'POST':
+        fm=loginform(request=request,data=request.POST)
+        if fm.is_valid():
+            uname = fm.cleaned_data['username']
+            upassword = fm.cleaned_data['password']
+            user = authenticate(username=uname, password=upassword)
+            if user is not None:
+                login(request, user)
+                messages.success(request,'Logged in successfully')
+                return HttpResponseRedirect('/dashboard/')
+    else:
+        fm = loginform()
+    return render(request,'blog/login.html',{'form':fm})
+    
